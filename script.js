d980d2a5e42e9b5157b23f2c6def502dd90c9597
ariohdekos <kosternoy20@gmail.com>
2026-02-17 15:26:16 +0200

    ?

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 // 1. КОНФІГУРАЦІЯ (Встав свої дані!)
 // ==========================================
 const firebaseConfig = {
-    apiKey: "ТВІЙ_API_KEY",
-    authDomain: "ТВІЙ_PROJECT.firebaseapp.com",
-    databaseURL: "ТВОЯ_URL_DATABASE",
-    projectId: "ТВІЙ_PROJECT_ID",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "..."
+  apiKey: "AIzaSyDZWcQ7INpnZj1Hbf0fICcsPs2Wndus8AM",
+  authDomain: "liceum-eit-manager.firebaseapp.com",
+  databaseURL: "https://liceum-eit-manager-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "liceum-eit-manager",
+  storageBucket: "liceum-eit-manager.firebasestorage.app",
+  messagingSenderId: "854455059262",
+  appId: "1:854455059262:web:e6282bed63182559c5a26f",
+  measurementId: "G-NKS31DZ3MK"
 };
 
 // Ініціалізація
